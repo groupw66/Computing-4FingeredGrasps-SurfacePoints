3e6ced4b94a2f7c3cd861a7dc2bb6011b39b361b
groupdoll <GroupDoll>
2014-10-20 16:19:35 +0700

    edit TestCompute4FingeredGrasps

--- test/TestCompute4FingeredGrasps.cpp
+++ test/TestCompute4FingeredGrasps.cpp
@@ -3,86 +3,53 @@
 #include <cmath>
 #include "Compute4FingeredGrasps.h"
 #include "PositionsNormalsFile.h"
+#include "OBJFile.h"
 #include "ObjectSurfacePoints.h"
 #include "SamplingPoints.h"
 
 namespace
 {
 
-inline void test_uniformAxis(char *filename, int npointsPerAxis, double halfAngle, int nUniqueSol)
+inline void test_uniformAxis(ObjectSurfacePoints &osp, int npointsPerAxis, double halfAngle, int nUniqueSol)
 {
-    PositionsNormalsFile obj(filename);
-    ObjectSurfacePoints osp(obj);
     std::vector<Eigen::Vector3d> sammplePoints = SamplingPoints::uniformAxis(osp.minAABB, osp.maxAABB, npointsPerAxis);
 
     std::vector<std::vector<Grasp> > sol;
     Compute4FingeredGrasps::compute4FingeredGrasps(sol, osp.surfacePoints, sammplePoints, halfAngle);
     std::vector<Grasp> uniqueSol;
     Compute4FingeredGrasps::uniqueSol(uniqueSol, sol);
-
+    /*
     std::vector<std::vector<Grasp> > sol_naive;
     Compute4FingeredGrasps::compute4FingeredGrasps_naive(sol_naive, osp.surfacePoints, sammplePoints, halfAngle);
     std::vector<Grasp> uniqueSol_naive;
     Compute4FingeredGrasps::uniqueSol(uniqueSol_naive, sol_naive);
-
     CHECK_EQUAL(uniqueSol_naive.size(), uniqueSol.size());
-    //CHECK_EQUAL(nUniqueSol, uniqueSol.size());
+    */
+    CHECK_EQUAL(nUniqueSol, uniqueSol.size());
 }
 
-TEST(compute4FingeredGrasps_ammo50_8PointsPerAxis)
-{
-    char filename[1000] = "meshes/spectralMesh/ammo50.txt";
-    int npointsPerAxis = 8;
-    double halfAngle = 10.d;
-    int nUniqueSol = 1646;
-
-    test_uniformAxis(filename, npointsPerAxis, halfAngle, nUniqueSol);
-}
-TEST(compute4FingeredGrasps_ammo50_16PointsPerAxis)
-{
-    char filename[1000] = "meshes/spectralMesh/ammo50.txt";
-    int npointsPerAxis = 16;
-    double halfAngle = 10.d;
-    int nUniqueSol = 5078;
-
-    test_uniformAxis(filename, npointsPerAxis, halfAngle, nUniqueSol);
-}
-TEST(compute4FingeredGrasps_ammo50_32PointsPerAxis)
-{
-    char filename[1000] = "meshes/spectralMesh/ammo50.txt";
-    int npointsPerAxis = 32;
-    double halfAngle = 10.d;
-    int nUniqueSol = 8073;
-
-    test_uniformAxis(filename, npointsPerAxis, halfAngle, nUniqueSol);
-}
-
-TEST(compute4FingeredGrasps_cow500_8PointsPerAxis)
-{
-    char filename[1000] = "meshes/spectralMesh/cow500.txt";
-    int npointsPerAxis = 8;
-    double halfAngle = 10.d;
-    int nUniqueSol = 12091;
-
-    test_uniformAxis(filename, npointsPerAxis, halfAngle, nUniqueSol);
-}
 TEST(compute4FingeredGrasps_cow500_16PointsPerAxis)
 {
     char filename[1000] = "meshes/spectralMesh/cow500.txt";
     int npointsPerAxis = 16;
     double halfAngle = 10.d;
-    int nUniqueSol = 52451;
+    int nUniqueSol = 35255;
 
-    test_uniformAxis(filename, npointsPerAxis, halfAngle, nUniqueSol);
+    PositionsNormalsFile obj(filename);
+    ObjectSurfacePoints osp(obj);
+    test_uniformAxis(osp, npointsPerAxis, halfAngle, nUniqueSol);
 }
-TEST(compute4FingeredGrasps_cow500_32PointsPerAxis)
-{
-    char filename[1000] = "meshes/spectralMesh/cow500.txt";
-    int npointsPerAxis = 32;
-    double halfAngle = 10.d;
-    int nUniqueSol = 137508;
 
-    test_uniformAxis(filename, npointsPerAxis, halfAngle, nUniqueSol);
+TEST(compute4FingeredGrasps_CatLying_800_8PointsPerAxis)
+{
+    char filename[1000] = "meshes/KIT/CatLying_800.obj";
+    int npointsPerAxis = 8;
+    double halfAngle = 10.d;
+    int nUniqueSol = 126939;
+
+    OBJFile obj(filename);
+    ObjectSurfacePoints osp(obj);
+    test_uniformAxis(osp, npointsPerAxis, halfAngle, nUniqueSol);
 }
 
 
