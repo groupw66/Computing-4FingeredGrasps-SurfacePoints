c39b8e0a5f87f23332a7846ad6d776f9ea6f27ff
groupdoll <GroupDoll>
2014-10-22 01:06:30 +0700

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,21 +7,44 @@
 #include "OBJFile.h"
 #include "PositionsNormalsFile.h"
 #include "ObjectSurfacePoints.h"
+#include <chrono>
 
 using namespace std;
+
+//https://gist.github.com/gongzhitaao/7062087
+class Timer
+{
+public:
+    Timer() : beg_(clock_::now()) {}
+    void reset() { beg_ = clock_::now(); }
+    double elapsed() const {
+        return std::chrono::duration_cast<second_>
+            (clock_::now() - beg_).count(); }
+
+private:
+    typedef std::chrono::high_resolution_clock clock_;
+    typedef std::chrono::duration<double, std::ratio<1> > second_;
+    std::chrono::time_point<clock_> beg_;
+};
 
 void runCompute4FingeredGrasps(std::string randomMode, std::string objFilename, std::string outFilename, int nSamplePoint, double halfAngle)
 {
     std::cout << outFilename << std::endl;
-    ObjectSurfacePoints osp;
-    std::vector<Eigen::Vector3d> sampledPoints;
-    std::set<Grasp> solSet;
-    std::vector<int> sizeSols;
+
+    // runtime
+    Timer tmr;
+    Timer tmr2;
+    double sampleRuntime = 0.d;
+    double pointInConesRuntimes = 0.d;
+    double findEquilibriumRuntimes = 0.d;
+    std::vector<double> eachSampleRuntimes;
+
     std::ofstream ofs;
     ofs.unsetf ( std::ios::floatfield );
     ofs.precision(std::numeric_limits<long double>::digits10);
 
     //open object model file
+    ObjectSurfacePoints osp;
     std::string objFileExt = objFilename.substr(objFilename.find_last_of(".") + 1);
     if(objFileExt == "obj") {
         OBJFile obj(objFilename.c_str());
@@ -34,19 +57,32 @@ void runCompute4FingeredGrasps(std::string randomMode, std::string objFilename, 
     else {
         std::cout << "Not supported file..." << std::endl;
     }
+    printf("Write .surfacePoints start\n");
+    ofs.open(outFilename + ".surfacePoints", std::ofstream::ate);
+    ofs << osp.surfacePoints.size() << "\n";
+    for(SurfacePoint sp : osp.surfacePoints){
+        ofs << sp.position.x() << " " << sp.position.y() << " " << sp.position.z() << " ";
+        ofs << sp.normal.x() << " " << sp.normal.y() << " " << sp.normal.z() << "\n";
+    }
+    ofs.close();
+    printf("Write .surfacePoints ok\n");
+
 
     // SamplingPoints
+    std::vector<Eigen::Vector3d> sampledPoints;
+    tmr.reset();
     if(randomMode == "-ru"){
-        outFilename;
         printf("SamplingPoints::randomUniform start\n");
         sampledPoints = SamplingPoints::randomUniform(osp.minAABB, osp.maxAABB, nSamplePoint);
         printf("SamplingPoints::randomUniform ok\n");
     }
     else if(randomMode == "-rn"){
         printf("SamplingPoints::randomNormalDist start\n");
-        sampledPoints = SamplingPoints::randomNormalDist(osp.minAABB, osp.maxAABB, nSamplePoint);
+        Eigen::Vector3d diffAABB = osp.maxAABB - osp.minAABB;
+        sampledPoints = SamplingPoints::randomNormalDist(osp.cm, diffAABB/6., nSamplePoint);
         printf("SamplingPoints::randomNormalDist ok\n");
     }
+    sampleRuntime = tmr.elapsed();
 
     printf("Write .sampledPoints start\n");
     ofs.open(outFilename + ".sampledPoints", std::ofstream::ate);
@@ -59,8 +95,50 @@ void runCompute4FingeredGrasps(std::string randomMode, std::string objFilename, 
 
     // Compute4FingeredGrasps
     printf("Compute4FingeredGrasps start\n");
-    sizeSols = Compute4FingeredGrasps::compute4FingeredGrasps(solSet, osp.surfacePoints, sampledPoints, halfAngle);
+    std::set<Grasp> solSet;
+    std::vector<int> sizeSols;
+    std::vector<std::vector<Grasp> > sols;
+    sols.resize(sampledPoints.size());
+    //sizeSols = Compute4FingeredGrasps::compute4FingeredGrasps(solSet, osp.surfacePoints, sampledPoints, halfAngle);
+    //#pragma omp parallel for schedule(static, 1)
+    tmr.reset();
+    for(unsigned int i=0 ; i<sampledPoints.size() ; ++i){
+        tmr2.reset();
+        std::vector<unsigned int> filteredPointIds;
+        Compute4FingeredGrasps::pointInConesFilter(filteredPointIds, osp.surfacePoints, sampledPoints[i], halfAngle);
+        pointInConesRuntimes += tmr2.elapsed();
+
+        tmr2.reset();
+        std::vector<Grasp> fcGrasps;
+        if(filteredPointIds.size() >= 4){
+            Compute4FingeredGrasps::findEquilibriumGrasps_forceDual(fcGrasps, filteredPointIds, sampledPoints[i], osp.surfacePoints);
+        }
+        findEquilibriumRuntimes += tmr2.elapsed();
+        //#pragma omp critical
+        {
+            //solSet.insert(fcGrasps.begin(),fcGrasps.end());
+            for(Grasp g : fcGrasps){
+                if(solSet.insert(g).second){
+                    sols[i].push_back(g);
+                }
+            }
+            sizeSols.push_back(solSet.size());
+        }
+        eachSampleRuntimes.push_back(tmr.elapsed());
+    }
     printf("Compute4FingeredGrasps ok\n");
+
+    printf("Write .runtime start\n");
+    ofs.open(outFilename + ".runtime", std::ofstream::ate);
+    ofs << "sampleRuntime: " << sampleRuntime << "\n";
+    ofs << "pointInConesRuntimes: " << pointInConesRuntimes << "\n";
+    ofs << "findEquilibriumRuntimes: " << findEquilibriumRuntimes << "\n";
+    ofs << "eachSampleRuntimes\n" << eachSampleRuntimes.size() << "\n";
+    for(double eachSampleRuntime : eachSampleRuntimes){
+        ofs << eachSampleRuntime << "\n";
+    }
+    ofs.close();
+    printf("Write .runtime ok\n");
 
     printf("Write .sizeSols start\n");
     ofs.open(outFilename + ".sizeSols", std::ofstream::ate);
@@ -71,11 +149,27 @@ void runCompute4FingeredGrasps(std::string randomMode, std::string objFilename, 
     ofs.close();
     printf("Write .sizeSols ok\n");
 
+    printf("Write .sols start\n");
+    ofs.open(outFilename + ".sols", std::ofstream::ate);
+    ofs << sols.size() << "\n";
+    for(std::vector<Grasp> sol : sols){
+        ofs << sol.size() << "\n";
+        for(Grasp g : sol){
+            for(unsigned int id : g.surfacePoints){
+                ofs << id << " ";
+            }
+            ofs << "\n";
+        }
+    }
+    ofs.close();
+    printf("Write .sols ok\n");
+
     printf("--------------------\n");
 }
 
 int main(int argc,char *argv[])
 {
+    // (-ru,-rn) objFilename outFilename nSamplePoint halfAngle
     if(argc > 1){
         std::string mode;
         if(argc >= 2){
