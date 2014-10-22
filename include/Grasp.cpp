38762207428183904de5f90412472442897169c6
Mana <mana_ohm@hotmail.com>
2014-10-22 11:11:09 +0700

    reserve() surfacePoints before push_back()

--- include/Grasp.cpp
+++ include/Grasp.cpp
@@ -13,6 +13,7 @@ Grasp::~Grasp()
 Grasp::Grasp(unsigned int sp1, unsigned int sp2, unsigned int sp3, unsigned int sp4)
 {
     surfacePoints.clear();
+    surfacePoints.reserve(4);
     surfacePoints.push_back(sp1);
     surfacePoints.push_back(sp2);
     surfacePoints.push_back(sp3);
@@ -24,6 +25,7 @@ Grasp::Grasp(unsigned int sp1, unsigned int sp2, unsigned int sp3, unsigned int 
 Grasp::Grasp(const std::vector<unsigned int>& _surfacePoints)
 {
     surfacePoints.clear();
+    surfacePoints.reserve(_surfacePoints.size());
     surfacePoints.insert(surfacePoints.end(), _surfacePoints.begin(), _surfacePoints.end());
     std::sort(surfacePoints.begin(), surfacePoints.end());
     //cm = _cm;
