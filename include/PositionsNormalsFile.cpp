430fa93d88ffa62d86607636e8244c67065f0476
groupdoll <GroupDoll>
2014-10-23 19:19:43 +0700

    add PositionsNormalsFile::write

--- include/PositionsNormalsFile.cpp
+++ include/PositionsNormalsFile.cpp
@@ -14,11 +14,13 @@ PositionsNormalsFile::PositionsNormalsFile(const char* _filename)
     open(_filename);
 }
 
-void PositionsNormalsFile::open(const char* _filename)
+bool PositionsNormalsFile::open(const char* _filename)
 {
     positions.clear();
     normals.clear();
     std::ifstream myfile(_filename);
+    if(!myfile.is_open())
+        return false;
     int n;
     myfile >> n;
     positions.reserve(n);
@@ -28,7 +30,33 @@ void PositionsNormalsFile::open(const char* _filename)
         myfile >> a >> b >> c >> d >> e >> f;
         Eigen::Vector3d p(a,b,c);
         positions.push_back(p);
-        Eigen::Vector3d n(-d,-e,-f);
+        //Eigen::Vector3d n(-d,-e,-f);
+        Eigen::Vector3d n(d,e,f);
         normals.push_back(n);
     }
+    myfile.close();
+    return true;
 }
+
+PositionsNormalsFile::PositionsNormalsFile(std::vector<Eigen::Vector3d> _positions, std::vector<Eigen::Vector3d> _normals)
+{
+    positions = _positions;
+    normals = _normals;
+}
+
+bool PositionsNormalsFile::write(const char* _filename)
+{
+    if(positions.size() != normals.size())
+        return false;
+    std::ofstream myfile(_filename);
+    if(!myfile.is_open())
+        return false;
+    myfile << positions.size() << "\n";
+    for(unsigned int i=0 ; i<positions.size() ; ++i){
+        myfile << positions[i].x() << " " << positions[i].y() << " " << positions[i].z() << " "
+                << normals[i].x() << " " << normals[i].y() << " " << normals[i].z() << "\n";
+    }
+    myfile.close();
+    return true;
+}
+
--- include/PositionsNormalsFile.h
+++ include/PositionsNormalsFile.h
@@ -12,7 +12,10 @@ class PositionsNormalsFile
         PositionsNormalsFile();
         virtual ~PositionsNormalsFile();
         PositionsNormalsFile(const char *_filename);
-        void open(const char *_filename);
+        bool open(const char *_filename);
+
+        PositionsNormalsFile(std::vector<Eigen::Vector3d> _positions, std::vector<Eigen::Vector3d> _normals);
+        bool write(const char *_filename);
 
         char filename[1000];
         std::vector<Eigen::Vector3d> positions;
