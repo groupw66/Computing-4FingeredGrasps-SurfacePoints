43efa12f8c13267b0e51b436812047061a0ea6e8
groupdoll <GroupDoll>
2014-10-23 21:01:27 +0700

    add PositionsNormalsFile::write

--- test/TestPositionsNormalsFile.cpp
+++ test/TestPositionsNormalsFile.cpp
@@ -14,4 +14,17 @@ TEST(PositionsNormalsFile_ammo50)
     CHECK_EQUAL(obj.positions.size(), obj.normals.size());
 }
 
+TEST(PositionsNormalsFile_write)
+{
+    PositionsNormalsFile obj("test/meshes/spectralMesh/ammo50.txt");
+    PositionsNormalsFile out(obj.positions, obj.normals);
+    out.write("test/tmp.txt");
+    PositionsNormalsFile readOut("test/tmp.txt");
+    CHECK_EQUAL(readOut.positions.size(), obj.positions.size());
+    CHECK_EQUAL(readOut.normals.size(), obj.normals.size());
+    CHECK_ARRAY_EQUAL(obj.positions, readOut.positions, obj.positions.size());
+    CHECK_ARRAY_EQUAL(obj.normals, readOut.normals, obj.normals.size());
+    std::remove("test/tmp.txt");
+}
+
 }
